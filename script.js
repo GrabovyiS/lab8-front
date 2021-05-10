1f0c432b53e4e33e423c06339e17914416a73df7
Saveliy Graboviy$ git config --global user.name Saveliy <graboviys@yandex.ru>
2021-05-10 17:18:48 +0300

    done;

---
+++ script.js
@@ -0,0 +1,14 @@
+let ham = document.getElementById('ham');
+line1 = document.getElementById('line1');
+line2 = document.getElementById('line2');
+line3 = document.getElementById('line3');
+ham.addEventListener('click', function(){
+    line1.classList.toggle('line1-animate');
+    line2.classList.toggle('line2-animate');
+    line3.classList.toggle('line3-animate');
+});
+let switcher = document.getElementById('switch');
+let lamp = document.getElementById('lamp');
+switcher.addEventListener('click', function(){
+    lamp.classList.toggle('light')
+})
